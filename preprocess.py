86753d7911479e837e6737f7c53c48ea4f7cbb3a
dsmlanu <aanita.gomberr@gmail.com>
2025-02-14 22:15:36 +0530

    create prepprocessing.py
    create prepprocessing.py

---
+++ preprocess.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+import joblib
